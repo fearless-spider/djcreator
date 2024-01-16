ede439a3170f289da58f237d0995f8528d9e9828
Przemysław Pająk <office@fearlessspider.com>
2024-01-16 13:35:02 +0100

    initial

---
+++ djcreator.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+gum style \
+	--foreground 201 --border-foreground 201 --border double \
+	--align center --width 50 --margin "1 2" --padding "2 4" \
+	'Django Creator .::. PAJAK'
+
+TYPE=$(gum choose "clean" "auth")
+
+if [ $TYPE = "clean" ]; then
+	gum confirm "It will create new python env with latest Django and it will create new project. Are you sure?"
+
+	if [ $? -eq 0 ]; then
+		python -m venv djvenv
+		./djvenv/bin/pip install --upgrade pip
+		./djvenv/bin/pip install django django-bootstrap-v5
+		PROJECT_NAME=$(gum input --placeholder "Name of Django project")
+		./djvenv/bin/django-admin startproject "$PROJECT_NAME"
+	fi
+elif [ $TYPE = "auth" ]; then
+	gum confirm "It will create new python env with latest Django and it will create new project with login and register sections. Are you sure?"
+
+	if [ $? -eq 0 ]; then
+		python -m venv djvenv
+		./djvenv/bin/pip install --upgrade pip
+		./djvenv/bin/pip install django django-bootstrap-v5 django-registration
+		PROJECT_NAME=$(gum input --placeholder "Name of Django project")
+		./djvenv/bin/django-admin startproject "$PROJECT_NAME"
+	fi
+fi
+
+gum style \
+	--foreground 201 --border-foreground 201 --border double \
+	--align center --width 50 --margin "1 2" --padding "2 4" \
+	'Done!'
